a91524b8b08b3b5658e01621aa0925eb95192ea3
rafian-git <jaberalrafian@gmail.com>
2021-10-13 13:14:39 +0600

    tested 200

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+
+export default function Page () {
+  return (
+    <Layout>
+      <h1>Technical Test for Remote Office</h1>
+      {/* <p>
+        This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
+      </p> */}
+    </Layout>
+  )
+}
